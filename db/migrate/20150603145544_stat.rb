d5c297b54e66fcd30218ff576572deb43cb55c63
Bella <woo.bella@gmail.com>
2015-06-03 15:17:28 -0400

    empty tables initiatied and blank models available

---
+++ db/migrate/20150603145544_stat.rb
@@ -0,0 +1,10 @@
+class Stat < ActiveRecord::Migration
+  def change
+  	create_table "stats" do |s|
+  		s.integer "player_x_id"
+  		s.integer "wins"
+  		s.integer "losses"
+  		s.datetime "created"
+  	end
+  end
+end
